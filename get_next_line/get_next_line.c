0921aa4311207e2e698e8b6ce7f5ee634f1808f6
rakyun1 <dbskim234@naver.com>
2024-11-10 22:22:59 +0900

    start

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -3,17 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rakim <fkrdbs234@naver.com>                +#+  +:+       +#+        */
+/*   By: rakim <rakim@student.42gyeongsan.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 12:08:42 by rakim             #+#    #+#             */
-/*   Updated: 2024/10/10 15:22:07 by rakim            ###   ########.fr       */
+/*   Updated: 2024/11/10 22:19:05 by rakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 char	*get_next_line(int fd)
 {
+    int idx;
+    static char buffer[BUFFER_SIZE];
+
+    read(fd, buffer, BUFFER_SIZE);
+    idx = 0;
+    while (buffer[idx])
+    {
+        printf("%c", buffer[idx++]);
+    }
 
 	
+    return NULL;
 }
---
+++ get_next_line/main.c
@@ -0,0 +1,7 @@
+#include "get_next_line.h"
+
+int main(void)
+{
+    get_next_line(0);
+    return 0;
+}
